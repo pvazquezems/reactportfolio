bf607c8984a7fc1841c650bbe20f203ac8bf1744
Pablo Vazquez <pvazquezems@gmail.com>
2020-08-25 20:17:27 -0700

    removed some gitignore

---
+++ build/precache-manifest.4e5becd4d29f17444dada532b83e2f98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe98195f2a292f762003265a7afaeb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f72c9cc11ff39f4b191c",
+    "url": "/static/css/main.e46eabfc.chunk.css"
+  },
+  {
+    "revision": "fea95cf6da2056a0cc5c",
+    "url": "/static/js/2.61689fc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.61689fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72c9cc11ff39f4b191c",
+    "url": "/static/js/main.63ec38ae.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  },
+  {
+    "revision": "b0dcd82d6029e6b38c3b901d4247fe9b",
+    "url": "/static/media/cleansafe.b0dcd82d.png"
+  },
+  {
+    "revision": "e2ad49744a371e86d2c400b9362dc8b8",
+    "url": "/static/media/directory.e2ad4974.png"
+  },
+  {
+    "revision": "d68d856e138378ca86801b1f4fe7b5f7",
+    "url": "/static/media/password.d68d856e.png"
+  },
+  {
+    "revision": "12ef108f76b7a332df295d9b580be42b",
+    "url": "/static/media/planner.12ef108f.png"
+  },
+  {
+    "revision": "c6cd278e46e07843886c63cb469b30ef",
+    "url": "/static/media/quiz.c6cd278e.png"
+  },
+  {
+    "revision": "36ded9967dc292e7afe76ae93428b44d",
+    "url": "/static/media/weather.36ded996.png"
+  }
+]);
